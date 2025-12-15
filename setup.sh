2e74eb640daf638687387125f8f2dc1fddb5b682
apjanco <apjanco@gmail.com>
2025-12-15 10:31:58 -0500

    add setup bash script

---
+++ setup.sh
@@ -0,0 +1,14 @@
+cd /scratch/network/$USER
+git clone https://github.com/PULdischo/vlms-on-hpc.git
+module load anaconda3/2025.6
+conda env create -f conda_env.yml
+
+rsync -avu $HOME/.conda /scratch/network/$USER/
+rm -Rf $HOME/.conda
+cd $HOME && ln -s /scratch/network/$USER/.conda .conda
+
+rsync -avu $HOME/.triton /scratch/network/$USER/
+rm -Rf $HOME/.triton
+cd $HOME && ln -s /scratch/network/$USER/.triton .triton
+echo "export HF_HOME=/scratch/network/$USER/.cache/huggingface/" >> $HOME/.bashrc
+source $HOME/.bashrc
